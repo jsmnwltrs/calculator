ff6a91b8e4b1b45dd3de9fd259a3bbaf03d7407c
Jasmine Walters <jsmnwltrs@gmail.com>
2018-09-29 11:46:01 -0500

    math all working

---
+++ javascripts/components/calculator.js
@@ -0,0 +1,25 @@
+import {printToDom} from '../helpers/util.js';
+import {multiply, divide, add, subtract} from '../helpers/math.js';
+
+const calculate = (num1, num2, mathType) => {
+    let answer = 0;
+switch(mathType){           //if else statement that is good for a condition that relies on one variable change
+    case "multiply":
+        answer = multiply(num1, num2);
+        break;
+    case "divide":
+       answer = divide(num1, num2);
+        break;
+    case "add":
+        answer = add(num1, num2);
+        break;
+    case "subtract":
+        answer = subtract(num1, num2);
+        break;
+    default:
+        answer = "none";
+}
+printToDom(answer, 'result')
+};
+
+export {calculate}
